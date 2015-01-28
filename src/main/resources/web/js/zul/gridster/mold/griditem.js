e5d5b94b68826a0d9ea13b1eb7181b8f2e2e1a49
Sean Connolly <connolly.st@gmail.com>
2015-01-28 15:28:49 +0100

    Added support for nested elements.

--- src/main/resources/web/js/zul/gridster/mold/griditem.js
+++ src/main/resources/web/js/zul/gridster/mold/griditem.js
@@ -1,3 +1,7 @@
 function (out) {
-    out.push('<li ', this.domAttrs_(), '></li>');
+    out.push('<li ', this.domAttrs_(), '>');
+    for (var w = this.firstChild; w; w = w.nextSibling) {
+        w.redraw(out);
+    }
+    out.push('</li>');
 }
--- src/test/java/org/zkoss/gridster/DemoViewModel.java
+++ src/test/java/org/zkoss/gridster/DemoViewModel.java
@@ -5,8 +5,12 @@ import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Window;
 
 /**
@@ -32,7 +36,23 @@ public class DemoViewModel {
 		item.setSizeX(1);
 		item.setSizeY(1);
 		item.setStyle("background-color: black;");
+		Button closeButton = new Button("Remove");
+		closeButton.addEventListener(Events.ON_CLICK, new CloseItemEventListener(item));
+		item.appendChild(closeButton);
 		dynamicGridster.appendChild(item);
 	}
 
+	private static final class CloseItemEventListener implements EventListener<Event> {
+
+		private final GridItem item;
+
+		private CloseItemEventListener(GridItem item) {
+			this.item = item;
+		}
+
+		@Override
+		public void onEvent(Event event) throws Exception {
+			item.detach();
+		}
+	}
 }
