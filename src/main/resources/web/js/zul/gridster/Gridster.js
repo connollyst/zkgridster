9475058a90bce52c14ee570e12118260a500be52
Sean Connolly <connolly.st@gmail.com>
2015-01-28 14:50:34 +0100

    Applied gridster on bind_()

--- src/main/resources/web/js/zul/gridster/Gridster.js
+++ src/main/resources/web/js/zul/gridster/Gridster.js
@@ -1,1 +1,6 @@
-zul.gridster.Gridster = zk.$extends(zk.Widget, {});
+zul.gridster.Gridster = zk.$extends(zk.Widget, {
+    bind_: function () {
+        this.$supers(zul.gridster.Gridster, 'bind_', arguments);
+        $('#' + this.uuid + ' ul').gridster();
+    }
+});
--- src/main/resources/web/js/zul/gridster/mold/gridster.js
+++ src/main/resources/web/js/zul/gridster/mold/gridster.js
@@ -1,6 +1,6 @@
 function (out) {
     out.push('<div', this.domAttrs_(), '>');
-    out.push('<ul>');
+    out.push('<ul style="list-style-type:none; padding:0; margin:0;">');
     for (var w = this.firstChild; w; w = w.nextSibling) {
         w.redraw(out);
     }
