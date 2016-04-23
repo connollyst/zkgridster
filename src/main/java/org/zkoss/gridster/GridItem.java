5d02afecdb94bed41b8bca2663f52974d421607b
Sean Connolly <connolly.st@gmail.com>
2016-04-23 18:07:43 -0400

    row, col, sizex, and sizey can now be null, letting gridster apply its default behavior

--- src/main/java/org/zkoss/gridster/GridItem.java
+++ src/main/java/org/zkoss/gridster/GridItem.java
@@ -12,114 +12,114 @@ import java.io.IOException;
  */
 public class GridItem extends XulElement {
 
-	private int row = 1;
-	private int col = 1;
-	private int sizeX = 1;
-	private int sizeY = 1;
-	private Integer maxSizeX = null;
-	private Integer maxSizeY = null;
-	private Integer minSizeX = null;
-	private Integer minSizeY = null;
+    private Integer row = null;
+    private Integer col = null;
+    private Integer sizeX = null;
+    private Integer sizeY = null;
+    private Integer maxSizeX = null;
+    private Integer maxSizeY = null;
+    private Integer minSizeX = null;
+    private Integer minSizeY = null;
 
-	public int getRow() {
-		return row;
-	}
+    public Integer getRow() {
+        return row;
+    }
 
-	public int getCol() {
-		return col;
-	}
+    public Integer getCol() {
+        return col;
+    }
 
-	public int getSizeX() {
-		return sizeX;
-	}
+    public Integer getSizeX() {
+        return sizeX;
+    }
 
-	public int getSizeY() {
-		return sizeY;
-	}
+    public Integer getSizeY() {
+        return sizeY;
+    }
 
-	public Integer getMaxSizeX() {
-		return maxSizeX;
-	}
+    public Integer getMaxSizeX() {
+        return maxSizeX;
+    }
 
-	public Integer getMaxSizeY() {
-		return maxSizeY;
-	}
+    public Integer getMaxSizeY() {
+        return maxSizeY;
+    }
 
-	public Integer getMinSizeX() {
-		return minSizeX;
-	}
+    public Integer getMinSizeX() {
+        return minSizeX;
+    }
 
-	public Integer getMinSizeY() {
-		return minSizeY;
-	}
+    public Integer getMinSizeY() {
+        return minSizeY;
+    }
 
-	public void setRow(int row) {
-		if(this.row != row) {
-			this.row = row;
-			smartUpdate("row", row);
-		}
-	}
+    public void setRow(int row) {
+        if (this.row == null || this.row != row) {
+            this.row = row;
+            smartUpdate("row", row);
+        }
+    }
 
-	public void setCol(int col) {
-		if(this.col != col) {
-			this.col = col;
-			smartUpdate("col", col);
-		}
-	}
+    public void setCol(int col) {
+        if (this.col == null || this.col != col) {
+            this.col = col;
+            smartUpdate("col", col);
+        }
+    }
 
-	public void setSizeX(int sizeX) {
-		if(this.sizeX != sizeX) {
-			this.sizeX = sizeX;
-			smartUpdate("sizeX", sizeX);
-		}
-	}
+    public void setSizeX(int sizeX) {
+        if (this.sizeX == null || this.sizeX != sizeX) {
+            this.sizeX = sizeX;
+            smartUpdate("sizeX", sizeX);
+        }
+    }
 
-	public void setSizeY(int sizeY) {
-		if(this.sizeY != sizeY) {
-			this.sizeY = sizeY;
-			smartUpdate("sizeY", sizeY);
-		}
-	}
+    public void setSizeY(int sizeY) {
+        if (this.sizeY == null || this.sizeY != sizeY) {
+            this.sizeY = sizeY;
+            smartUpdate("sizeY", sizeY);
+        }
+    }
 
-	public void setMaxSizeX(int maxSizeX) {
-		if(this.maxSizeX == null || this.maxSizeX != maxSizeX) {
-			this.maxSizeX = maxSizeX;
-			smartUpdate("maxSizeX", maxSizeX);
-		}
-	}
+    public void setMaxSizeX(int maxSizeX) {
+        if (this.maxSizeX == null || this.maxSizeX != maxSizeX) {
+            this.maxSizeX = maxSizeX;
+            smartUpdate("maxSizeX", maxSizeX);
+        }
+    }
 
-	public void setMaxSizeY(int maxSizeY) {
-		if(this.maxSizeY == null || this.maxSizeY != maxSizeY) {
-			this.maxSizeY = maxSizeY;
-			smartUpdate("maxSizeY", maxSizeY);
-		}
-	}
+    public void setMaxSizeY(int maxSizeY) {
+        if (this.maxSizeY == null || this.maxSizeY != maxSizeY) {
+            this.maxSizeY = maxSizeY;
+            smartUpdate("maxSizeY", maxSizeY);
+        }
+    }
 
-	public void setMinSizeX(int minSizeX) {
-		if(this.minSizeX == null || this.minSizeX != minSizeX) {
-			this.minSizeX = minSizeX;
-			smartUpdate("minSizeX", minSizeX);
-		}
-	}
+    public void setMinSizeX(int minSizeX) {
+        if (this.minSizeX == null || this.minSizeX != minSizeX) {
+            this.minSizeX = minSizeX;
+            smartUpdate("minSizeX", minSizeX);
+        }
+    }
 
-	public void setMinSizeY(int minSizeY) {
-		if(this.minSizeY == null || this.minSizeY != minSizeY) {
-			this.minSizeY = minSizeY;
-			smartUpdate("minSizeY", minSizeY);
-		}
-	}
+    public void setMinSizeY(int minSizeY) {
+        if (this.minSizeY == null || this.minSizeY != minSizeY) {
+            this.minSizeY = minSizeY;
+            smartUpdate("minSizeY", minSizeY);
+        }
+    }
 
-	@Override
-	protected void renderProperties(ContentRenderer renderer) throws IOException {
-		super.renderProperties(renderer);
-		render(renderer, "row", row);
-		render(renderer, "col", col);
-		render(renderer, "sizeX", sizeX);
-		render(renderer, "sizeY", sizeY);
-		render(renderer, "maxSizeX", maxSizeX);
-		render(renderer, "maxSizeY", maxSizeY);
-		render(renderer, "minSizeX", minSizeX);
-		render(renderer, "minSizeY", minSizeY);
-	}
+    @Override
+    protected void renderProperties(ContentRenderer renderer) throws IOException {
+        super.renderProperties(renderer);
+        render(renderer, "row", row);
+        render(renderer, "col", col);
+        render(renderer, "sizeX", sizeX);
+        render(renderer, "sizeY", sizeY);
+        render(renderer, "maxSizeX", maxSizeX);
+        render(renderer, "maxSizeY", maxSizeY);
+        render(renderer, "minSizeX", minSizeX);
+        render(renderer, "minSizeY", minSizeY);
+    }
 
 }
